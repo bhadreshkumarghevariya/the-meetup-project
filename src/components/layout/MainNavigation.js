dc22b18128421d7e8fc0b67c4e98430933e873b1
BHADRESHKUMAR GHEVARIYA <bhadreshkumarghevariya@gmail.com>
2022-01-07 11:20:43 +0530

    favorites functionality completed using context

--- src/components/layout/MainNavigation.js
+++ src/components/layout/MainNavigation.js
@@ -1,8 +1,12 @@
+import { useContext } from "react";
 import { Link } from "react-router-dom";
 
 import classes from "./MainNavigation.module.css";
+import FavoritesContext from "../../store/favorites-context";
 
 function MainNavigation() {
+  const favoriteCtx = useContext(FavoritesContext);
+
   return (
     <header className={classes.header}>
       <div className={classes.logo}>React Meetups</div>
@@ -15,7 +19,12 @@ function MainNavigation() {
             <Link to="/new-meetup">New Meetup</Link>
           </li>
           <li>
-            <Link to="/favorits">Favorits</Link>
+            <Link to="/favorits">
+              Favorits{" "}
+              <span className={classes.badge}>
+                {favoriteCtx.totalFavorites}
+              </span>
+            </Link>
           </li>
         </ul>
       </nav>
